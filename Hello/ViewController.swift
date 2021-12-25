6ed8e2fc5a481502c5fdc8f918b038120b0808dd
Song San <songsan2012@gmail.com>
2021-12-25 09:43:19 -0800

    Add Hello button with multiple languages

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        helloLabel.text = ""
     }
 
 
+    @IBAction func sayHelloPressed(_ sender: Any) {
+        
+        let englishHello = "Hello"
+        let spanishHello = "Hola"
+        let chineseHello = "Ni Hao"
+        
+        if helloLabel.text == englishHello {
+            helloLabel.text = spanishHello
+        }
+        else if helloLabel.text == spanishHello {
+            helloLabel.text = chineseHello
+        }
+        else
+        {
+            helloLabel.text = englishHello
+        }
+        
+        
+    }
+    
 }
 
